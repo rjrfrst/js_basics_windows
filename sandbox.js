2b8f2120e7430b828c915de63e3b0f32d1ad2aca
rjrfrst <rjrfrst@gmail.com>
2023-12-16 11:42:57 +0000

    Made changes and learned about switch cases

--- sandbox.js
+++ sandbox.js
@@ -83,3 +83,44 @@
 // console.log(!false);
 // console.log(!true); 
 
+// -----> Chapter 3.8 Break & Continue <-----
+// const scores = [50, 25, 0, 30, 100, 20, 10];
+
+// for (let i = 0; i < scores.length; i++){
+    
+//     if(scores[i] === 0){
+//         continue;
+//     }
+
+//     console.log('your score:', scores[i]);
+//     if(scores[i] === 100){
+//         console.log('Congrats, you are 100!');
+//         break;
+//     }
+
+// }
+
+// -----> Chapter 3.9 Switch statements <-----
+// const grade ='D';
+// switch(grade){
+//     case 'A':
+//         console.log('you got an A');
+//         break;
+//     case 'B':
+//         console.log('you got an B');
+//         break;
+//     case 'C':
+//         console.log('you got an C');
+//         break;
+//     case 'D':
+//         console.log('you got an D');
+//         break;
+//     case 'E':
+//         console.log('you got an E');
+//         break;
+//     default:
+//         console.log('not a valid grade.')
+
+// }
+
+// -----> Chapter 3.10 Variables and Block Scope <-----
