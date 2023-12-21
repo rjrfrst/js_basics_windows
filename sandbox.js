a338da309411a1eabaff6a26ee201e88233973ba
rjrfrst <rjrfrst@gmail.com>
2023-12-21 11:33:16 +0000

    finished chapter 3

--- sandbox.js
+++ sandbox.js
@@ -124,3 +124,19 @@
 // }
 
 // -----> Chapter 3.10 Variables and Block Scope <-----
+// The same rules apply with let.
+// const age = 30;
+
+// if (true){
+//     const age = 40;
+//     const name = 'Mario';
+//     console.log('inside 1st code block: ', age, name);
+
+//     if(true){
+//         const age = 50;
+//         console.log('inside 2nd code block', age);
+//         var test = 'hello'
+//     }
+// }
+
+// console.log('outside code block: ', age);
